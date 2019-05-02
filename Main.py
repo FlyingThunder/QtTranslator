faedba86bc1de49f9bb89afb17d8cd34dd28b6d0
FlyingThunder <laurens.peschko@gmail.com>
2019-05-02 10:35:50 +0200

    added two settings

--- Main.py
+++ Main.py
@@ -6,7 +6,9 @@ import googletrans
 import BrowseFileSystem
 import re
 import os
+import time
 from tika import parser
+
 
 class Translator(QtWidgets.QMainWindow, QtOutput.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -46,6 +48,18 @@ class Translator(QtWidgets.QMainWindow, QtOutput.Ui_MainWindow):
     def Clean_Textbox(self):
         self.Text_Input.clear()
         self.Text_Output.clear()
+        try:
+            print(self.dialog.setting1var)
+        except:
+            print("nicht zugewiesen")
+
+    def saveOutput(self):
+        resultname = self.Text_Output.toPlainText()[0:10]
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        with open(resultname+"_"+timestr+".txt", "w") as x:
+            x.write(self.Text_Output.toPlainText())
+        x.close()
+
 
     def Translate_Function(self):                                               # einfügen von text, anpassen von comboboxen bei übersetzung
         inputText = self.Text_Input.toPlainText()
@@ -54,6 +68,8 @@ class Translator(QtWidgets.QMainWindow, QtOutput.Ui_MainWindow):
             OutputText = self.TranslateText(InputTranslate=inputText, InputLanguage=self.ComboInputValue, OutputLanguage=self.ComboOutputValue)
             self.Combo_Input.setCurrentText(str(self.TranslatedSource))
             self.Text_Output.append(OutputText)
+            if self.dialog.setting2var == 1:
+                self.saveOutput()
 
     def TranslateText(self, InputTranslate, InputLanguage, OutputLanguage):      # eigentliche "übersetzung" findet hier statt
         output = self.TranslatorObj.translate(src=InputLanguage, dest=OutputLanguage, text=InputTranslate)
@@ -102,7 +118,13 @@ class Translator(QtWidgets.QMainWindow, QtOutput.Ui_MainWindow):
     def getPDF(self):                       #pdf auslesen
         raw = parser.from_file("temp.pdf")
         self.output = raw['content']            #plaintext auslesen
-        self.Text_Input.insertPlainText(re.sub(r'(\n)\1+', r'\1', self.output)[0:4999])             #plaintext formatieren (leere zeilen und umbrüche formatieren und auf max. 5000 zeichen begrenzen)
+        self.Text_Input.clear()
+        self.cutOutput = re.sub(r'(\n)\1+', r'\1', self.output)
+        if self.dialog.setting1var == 1:                                    #einstellung, um URLs aus dem text zu entfernen
+            self.cutURLOutput = re.sub('http[^\n]+\n', '', self.cutOutput)
+            self.Text_Input.insertPlainText(self.cutURLOutput[0:4999])
+        else:
+            self.Text_Input.insertPlainText(self.cutOutput[0:4999])       #plaintext formatieren (leere zeilen und umbrüche formatieren und auf max. 5000 zeichen begrenzen)
 
 
     def getWebsite(self, site):             #webseite als pdf speichern
@@ -129,11 +151,35 @@ class Translator(QtWidgets.QMainWindow, QtOutput.Ui_MainWindow):
         except:
             pass
 
-
-class ShowSettings(QtWidgets.QMainWindow, Settings.Ui_Form):
+class ShowSettings(Settings.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(ShowSettings, self).__init__(parent)
         self.setupUi(self)
+        self.setting1var = 0
+        self.setting2var = 0
+        self.checkBox.stateChanged.connect(self.setting1)
+        self.checkBox_2.stateChanged.connect(self.setting2)
+        self.checkBox_3.stateChanged.connect(self.setting3)
+        self.checkBox_4.stateChanged.connect(self.setting4)
+
+    def setting1(self):
+        if self.checkBox.isChecked():
+            self.setting1var = 1
+        else:
+            self.setting1var = 0
+
+    def setting2(self):
+        if self.checkBox_2.isChecked():
+            self.setting2var = 1
+        else:
+            self.setting2var = 0
+
+    def setting3(self):
+        pass
+
+    def setting4(self):
+        pass
+
 
 
 def main():
